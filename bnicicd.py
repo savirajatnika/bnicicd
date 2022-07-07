7d9006892278ae62b3ddd9442e48316e7908b177
savirajatnika <deuisnjlx@gmail.com>
2022-07-07 14:25:34 +0700

    testing4

--- bnicicd.py
+++ bnicicd.py
@@ -1,8 +1,12 @@
 import requests 
+from datetime import datetime
 
 print ("Hello world")
 print ("testng CI CD BNI") 
 
 response = requests.get("https://google.com")
 
-print (response.text) 
+waktu = datetime.now()
+
+with open ("tempResponse/"+str(waktu)+".txt","w")  as f:
+    f.write(response.text)
